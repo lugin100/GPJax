7431b67e00741d62aa96687a938ff25b492722cf
Luis Gindorf <luis.gindorf@gmail.com>
2026-06-01 16:52:33 +0200

    Fix matern32 jvp for anisotropic lengthscale

--- gpjax/kernels/stationary/matern32.py
+++ gpjax/kernels/stationary/matern32.py
@@ -74,25 +74,30 @@ def matern32_kernel_jvp(primals, tangents):
     x, y, lengthscale, variance = primals
     x_dot, y_dot, l_dot, v_dot = tangents
 
-    diff = x - y
+    diff = (x - y)/ lengthscale
     # Add eps to avoid r = 0, which would make 2nd derivative undefined
     eps = 1e-12
-    r = jnp.sqrt(jnp.sum(diff**2) + eps)
-    tau = r / lengthscale
+    r = jnp.sqrt(jnp.sum(diff**2, axis=-1) + eps)
+    tau = r
 
     exp_term = jnp.exp(-jnp.sqrt(3.0) * tau)
 
     primal_out = variance * (1.0 + jnp.sqrt(3.0) * tau) * exp_term
 
-    dk_dx = -3.0 * variance * exp_term * diff / lengthscale**2
+    dk_dx = -3.0 * variance * exp_term * diff / lengthscale
     dk_dy = -dk_dx
-    dk_dl = 3.0 * variance* exp_term * tau**2 / lengthscale
+    dk_dl = 3.0 * variance * exp_term * diff**2 / lengthscale
+    # diff might have D>1 even if lengthsclae is scalar (isotropic)
+    if lengthscale.ndim == 0:
+        dk_dl = jnp.sum(dk_dl)
     dk_dv = (1.0 + jnp.sqrt(3.0) * tau) * exp_term
-
+    print(dk_dl.shape)
+    print(l_dot.shape)
+    print(jnp.dot(dk_dl, l_dot).shape)
     tangent_out = (
         jnp.dot(dk_dx, x_dot)
         + jnp.dot(dk_dy, y_dot)
-        + dk_dl * l_dot
+        + jnp.dot(dk_dl, l_dot)
         + dk_dv * v_dot
     )
     return primal_out, tangent_out
