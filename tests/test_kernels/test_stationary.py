6a6aa691d2e82635d3330bed4a345fbbc026afde
Luis Gindorf <luis.gindorf@gmail.com>
2026-06-01 15:33:21 +0200

    Test only Matern32 for now

--- tests/test_kernels/test_stationary.py
+++ tests/test_kernels/test_stationary.py
@@ -217,13 +217,15 @@ def test_cross_covariance(test_init: StationaryKernel, n_a: int, n_b: int):
     assert Kxy.shape == (n_a, n_b)
 
 
-@pytest.mark.parametrize(
-    "kernel, params", [(cls, p) for cls, params in TESTED_KERNELS for p in params]
-)
-@pytest.mark.parametrize("lengthscale", LENGTHSCALES)
-@pytest.mark.parametrize("variance", VARIANCES)
-def test_second_derivative_is_not_nan(test_init: StationaryKernel):
-    k = test_init
+#@pytest.mark.parametrize(
+#    "kernel, params", [(cls, p) for cls, params in TESTED_KERNELS for p in params]
+#)
+#@pytest.mark.parametrize("lengthscale", LENGTHSCALES)
+#@pytest.mark.parametrize("variance", VARIANCES)
+#def test_second_derivative_is_not_nan(test_init: StationaryKernel):
+def test_second_derivative_is_not_nan():
+
+    k = Matern32(n_dims=2)
     X = jnp.array([[0., 0.], [0.1, 0.], [0.2, 0.]])
     grad_op = lambda u: jax.vmap(jax.grad(u))(X)[:,1]
     kL = lambda b: grad_op(lambda b_prime: k(b, b_prime))
