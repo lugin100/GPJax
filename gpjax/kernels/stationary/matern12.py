875fe996a156bad0f2408c9bfc77338ac6ba7009
Luis Gindorf <luis.gindorf@gmail.com>
2026-06-01 19:34:18 +0200

    Implement analytic gradient for Matern12

--- gpjax/kernels/stationary/matern12.py
+++ gpjax/kernels/stationary/matern12.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
+import jax
 import jax.numpy as jnp
 from jaxtyping import Float
 import numpyro.distributions as npd
@@ -42,11 +43,65 @@ class Matern12(StationaryKernel):
 
     name: str = "Matérn12"
 
-    def __call__(self, x: Float[Array, " D"], y: Float[Array, " D"]) -> ScalarFloat:
-        x = self.slice_input(x) / _val(self.lengthscale)
-        y = self.slice_input(y) / _val(self.lengthscale)
-        K = _val(self.variance) * jnp.exp(-euclidean_distance(x, y))
-        return K.squeeze()
+    def __call__(
+        self,
+        x: Float[Array, " D"],
+        y: Float[Array, " D"],
+    ) -> Float[Array, ""]:
+        out = matern12_kernel(
+            self.slice_input(x),
+            self.slice_input(y),
+            _val(self.lengthscale),
+            _val(self.variance))
+        return out
+
+
+    @property
+    def spectral_density(self) -> npd.StudentT:
+        return build_student_t_distribution(nu=3)
+
+
+@jax.custom_jvp
+def matern12_kernel(x, y, lengthscale, variance):
+    x = x / lengthscale
+    y = y / lengthscale
+    tau = euclidean_distance(x, y)
+    K = variance * jnp.exp(-tau)
+    return K.squeeze()
+
+
+@matern12_kernel.defjvp
+def matern12_kernel_jvp(primals, tangents):
+    x, y, lengthscale, variance = primals
+    x_dot, y_dot, l_dot, v_dot = tangents
+
+    x = x / lengthscale
+    y = y / lengthscale
+    # Clip with eps to avoid tau = 0, which would make 2nd derivative unstable
+    eps = 1e-12
+    tau = jnp.maximum(euclidean_distance(x, y), eps)
+
+    exp_term = jnp.exp(-tau)
+
+    primal_out = variance * exp_term
+
+    dk_dx = -variance * exp_term * (x - y) / (lengthscale**2 * tau)
+    dk_dy = -dk_dx
+    dk_dv = exp_term
+    dk_dl = variance * exp_term * tau / lengthscale
+    # For n_dim>1, lengthscale can still be scalar (isotropic)
+    # In this case, sum up derivatives along dimension
+    if lengthscale.ndim == 0:
+        dk_dl = jnp.sum(dk_dl)
+
+    tangent_out = (
+        jnp.dot(dk_dx, x_dot)
+        + jnp.dot(dk_dy, y_dot)
+        + jnp.dot(dk_dl, l_dot)
+        + dk_dv * v_dot     # variance is always scalar
+    )
+    return primal_out, tangent_out
+
 
     @property
     def spectral_density(self) -> npd.StudentT:
