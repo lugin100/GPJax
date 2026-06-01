992f6b02fe582dd93d7acc84fa922543ab53e02a
Luis Gindorf <luis.gindorf@gmail.com>
2026-06-01 10:54:47 +0200

    First implementation of analytic gradient for Matern32

--- gpjax/kernels/stationary/matern32.py
+++ gpjax/kernels/stationary/matern32.py
@@ -13,6 +13,7 @@
 # limitations under the License.
 # ==============================================================================
 
+import jax
 import jax.numpy as jnp
 from jaxtyping import Float
 import numpyro.distributions as npd
@@ -25,7 +26,6 @@ from gpjax.kernels.stationary.utils import (
 )
 from gpjax.typing import Array
 
-
 class Matern32(StationaryKernel):
     r"""The Matérn kernel with smoothness parameter fixed at 1.5.
 
@@ -33,7 +33,7 @@ class Matern32(StationaryKernel):
     lengthscale parameter $\ell$ and variance $\sigma^2$.
 
     $$
-    k(x, y) = \sigma^2 \exp \Bigg(1+ \frac{\sqrt{3}\lvert x-y \rvert}{\ell} \ \Bigg)\exp\Bigg(-\frac{\sqrt{3}\lvert x-y\rvert}{\ell^2} \Bigg)
+    k(x, y) = \sigma^2 \exp \Bigg(1+ \frac{\sqrt{3}\lvert x-y \rvert}{\ell} \ \Bigg)\exp\Bigg(-\frac{\sqrt{3}\lvert x-y\rvert}{\ell} \Bigg)
     $$
     """
 
@@ -44,15 +44,54 @@ class Matern32(StationaryKernel):
         x: Float[Array, " D"],
         y: Float[Array, " D"],
     ) -> Float[Array, ""]:
-        x = self.slice_input(x) / _val(self.lengthscale)
-        y = self.slice_input(y) / _val(self.lengthscale)
-        tau = euclidean_distance(x, y)
-        K = (
-            _val(self.variance)
-            * (1.0 + jnp.sqrt(3.0) * tau)
-            * jnp.exp(-jnp.sqrt(3.0) * tau)
-        )
-        return K.squeeze()
+        out = matern32_kernel(
+            self.slice_input(x),
+            self.slice_input(y),
+            _val(self.lengthscale),
+            _val(self.variance))
+        print(out)
+        return out
+
+@jax.custom_jvp
+def matern32_kernel(x, y, lengthscale, variance):
+    x = x / lengthscale
+    y = y / lengthscale
+    tau = euclidean_distance(x, y)
+    K = (
+        variance
+        * (1.0 + jnp.sqrt(3.0) * tau)
+        * jnp.exp(-jnp.sqrt(3.0) * tau)
+    )
+    return K.squeeze()
+
+
+@matern32_kernel.defjvp
+def matern32_kernel_jvp(primals, tangents):
+    x, y, lengthscale, variance = primals
+    x_dot, y_dot, l_dot, v_dot = tangents
+
+    diff = x - y
+    # Add eps to avoid r = 0, which would make 2nd derivative undefined
+    eps = 1e-12
+    r = jnp.sqrt(jnp.sum(diff**2) + eps)
+    tau = r / lengthscale
+
+    exp_term = jnp.exp(-jnp.sqrt(3.0) * tau)
+
+    primal_out = variance * (1.0 + jnp.sqrt(3.0) * tau) * exp_term
+
+    dk_dx = -3.0 * variance * exp_term * diff / lengthscale**2
+    dk_dy = -dk_dx
+    dk_dl = 3.0 * variance* exp_term * tau**2 / lengthscale
+    dk_dv = (1.0 + jnp.sqrt(3.0) * tau) * exp_term
+
+    tangent_out = (
+        jnp.dot(dk_dx, x_dot)
+        + jnp.dot(dk_dy, y_dot)
+        + dk_dl * l_dot
+        + dk_dv * v_dot
+    )
+    return primal_out, tangent_out
 
     @property
     def spectral_density(self) -> npd.StudentT:
