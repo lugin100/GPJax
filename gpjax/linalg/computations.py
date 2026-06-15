dfa9cc5996cd73c9781012c76a6d3e42bb83f4b6
Luis Gindorf <luis.gindorf@gmail.com>
2026-06-15 13:12:04 +0200

    Remove solve_triangular based on mv because it is slow and superceeded

--- gpjax/linalg/computations.py
+++ gpjax/linalg/computations.py
@@ -24,40 +24,6 @@ def solve_triangular(L, b, lower=True):
     else: # b.ndim == 2
         return jax.vmap(solve, in_axes=1, out_axes=1)(b)
 
-def mv_triangular_solve(L: lx.AbstractLinearOperator, b, lower):
-    r"""Compute the linear system Lx = b using forward or backward substitution.
-
-    Args:
-        L: Triangular linear operator.
-        b: Right-hand side vector.
-        lower: Whether L is lower or upper triangular.
-    Returns:
-        x: Solution of Lx = b.
-    """
-    b = jnp.asarray(b)
-    n = b.shape[0]
-
-    def row(i):
-        e = jax.nn.one_hot(i, n, dtype=b.dtype)
-        return L.transpose().mv(e)
-
-    def body(m, x):
-            rhs = b[m]
-            row_m = row(m)
-            rhs = rhs - jnp.dot(row_m, x)
-            diag = row_m[m]
-            return x.at[m].set(rhs / diag)
-    x0 = jnp.zeros_like(b)
-
-    if lower:
-        return jax.lax.fori_loop(0, n, body, x0)
-    else:
-        def reverse_body(k, x):
-            m = n - 1 - k
-            return body(m, x)
-
-        return jax.lax.fori_loop(0, n, reverse_body, x0)
-
 
 def solve_block_triangular(L11, L21, L22, b1, b2):
     r"""Compute $(L L^T)^{-1} b$ where 
